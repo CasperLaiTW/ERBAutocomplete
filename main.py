a617355cd5c79d0dadfb96cc5caea5bedd644294
Casper Lai <casper.lai@sleepingdesign.com>
2014-01-28 15:52:41 +0800

    Add keymap bind that unmark comment add inline code. Update version.

--- main.py
+++ main.py
@@ -98,6 +98,23 @@ class MarkCommentCommand(sublime_plugin.TextCommand):
 
         replace_region = sublime.Region(start, end)
         self.view.replace(edit, replace_region, '<%#')
+class UnmarkCommentCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        target = '<%#'
+        lines = ERBComplete.get_lines_text(self.view)
+        reg = re.search('<%#', lines)
+        if(reg == None): return
+        idx = reg.start()
+
+        # get current select line
+        sel = self.view.sel()
+        region = sel[0]
+        sel_line = self.view.line(region)
+        start = sel_line.a + idx
+        end = start + len(target)
+
+        replace_region = sublime.Region(start, end)
+        self.view.replace(edit, replace_region, '<%')
 
 # Init
 def init():
