1f9e46f7be98444380264d11bd5436bd1f18238c
Casper Lai <casper.lai@sleepingdesign.com>
2014-01-28 15:43:26 +0800

    Add keymap bind that indicates comment add inline code. Update version

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import sublime, sublime_plugin
 import os
+import sys
 import re
+
+
 ERBCOMPLETIONS_SETTING = 'ERBAutocomplete.sublime-settings'
 ERBBASECOMPLETIONS_SETTING = 'ERBBasecomplete.sublime-settings'
 BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 PACKAGES_PATH = sublime.packages_path() or os.path.dirname(BASE_PATH)
 ERB_GRAMMAR = 'Packages/%s/erb.tmLanguage' % os.path.basename(BASE_PATH).replace('.sublime-package', '')
-
+is_python3 = sys.version_info[0] > 2
+ERBComplete = None
+# API
 class ERBAutoCompleteAPI():
     def init(self):
         settings = sublime.load_settings(ERBCOMPLETIONS_SETTING).get('customCompletions')
@@ -17,13 +22,7 @@ class ERBAutoCompleteAPI():
         self.customWords = customWords
         self.words = []
         self.words = sublime.load_settings(ERBBASECOMPLETIONS_SETTING)
-
-class ERBAutocompleteListener(sublime_plugin.EventListener):
-    def on_query_completions(self, view, prefix, locations):
-        self.completions = []
-        specialkey = False
-        scope = ERBComplete.words.get('scope')
-        
+    def get_line_text(self, view):
         sel = view.sel()
         region = sel[0]
         line = view.line(region)
@@ -31,7 +30,25 @@ class ERBAutocompleteListener(sublime_plugin.EventListener):
         end = sel[0].a
         cursor = sublime.Region(start, end)
         cursorText = view.substr(cursor)
-        temp = re.split("[^\w\.<]", cursorText)
+        temp = re.split("[^\w\.\%<]", cursorText)
+        return temp
+    def get_lines_text(self, view):
+        sel = view.sel()
+        region = sel[0]
+        line = view.line(region)
+        start = line.a
+        end = line.b
+        cursor = sublime.Region(start, end)
+        cursorText = view.substr(cursor)
+        return cursorText
+# Completion
+class ERBAutocompleteListener(sublime_plugin.EventListener):
+    def on_query_completions(self, view, prefix, locations):
+        self.completions = []
+        specialkey = False
+        scope = ERBComplete.words.get('scope')
+        
+        temp = ERBComplete.get_line_text(view)
         lineText = temp[-1]
 
         specialkey = True if lineText.find("<") >= 0 else False
@@ -61,7 +78,37 @@ class ERBAutocompleteListener(sublime_plugin.EventListener):
                 print("Switched syntax to: ERB")
             except:
                 pass
-            
-ERBComplete = ERBAutoCompleteAPI()
-def plugin_loaded():
+
+class IndicatesCommentCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        target = '<%'
+        
+        # find target
+        lines = ERBComplete.get_lines_text(self.view)
+        reg = re.search('<%(=|\s+)', lines)
+        if(reg == None): return
+        idx = reg.start()
+
+        # get current select line
+        sel = self.view.sel()
+        region = sel[0]
+        sel_line = self.view.line(region)
+        start = sel_line.a + idx
+        end = start + len(target)
+
+        replace_region = sublime.Region(start, end)
+        self.view.replace(edit, replace_region, '<%#')
+
+# Init
+def init():
+    globals()['ERBComplete'] = ERBAutoCompleteAPI()
     ERBComplete.init()
+
+def plugin_loaded():
+    sublime.set_timeout(init, 200)
+
+##################
+# Init plugin
+if not is_python3:
+    init()
+
