97aaed1ba4510ace7480ece49a45625ba0eac427
Casper Lai <casper.lai@sleepingdesign.com>
2014-03-06 11:51:20 +0800

    Added get partials folder. Issue with #4

--- library/apis/core.py
+++ library/apis/core.py
@@ -2,13 +2,14 @@ import sublime, sublime_plugin
 import os
 import sys
 import re
+import json
 
 ERBCOMPLETIONS_SETTING = 'ERBAutocomplete.sublime-settings'
 ERBBASECOMPLETIONS_SETTING = 'ERBBasecomplete.sublime-settings'
 BASE_PATH = os.path.abspath(os.path.dirname(__file__)) if os.path.isfile(os.path.dirname(__file__)) else os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 PACKAGES_PATH = sublime.packages_path() or os.path.dirname(BASE_PATH)
 ERB_GRAMMAR = 'Packages/%s/erb.tmLanguage' % os.path.basename(BASE_PATH).replace('.sublime-package', '')
-
+PARTIAL_FOLDER = 'partials'
 # API
 class Core():
     def __init__(self):
@@ -63,6 +64,23 @@ class Core():
                         return base
                 return os.path.normcase(project)
         return None
+
+    def get_partial_path(self, path):
+        if path is None:
+            return None
+        project_dir = self.get_project_path(path)
+        base_file = os.path.join(project_dir, '.base')
+
+        if os.path.isfile(base_file):
+            try:
+                with open(base_file) as data_file:
+                    configs = json.load(data_file)
+                return os.path.join(project_dir, configs['partials'])
+            except Exception:
+                pass
+                return os.path.join(project_dir, PARTIAL_FOLDER)
+        return os.path.join(project_dir, PARTIAL_FOLDER)
+
     def is_erb_file(self, path):
         if re.search('^[a-zA-Z0-9\-\_\.]*\.html\.erb$', os.path.basename(path)) is None:
             return False
