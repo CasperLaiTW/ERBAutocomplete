5a480f98fbfe50f1845daeb035713069073911b2
Casper Lai <casper.lai@sleepingdesign.com>
2014-04-07 00:34:01 +0800

    Fixed mapping layout path fail.

--- library/commands/mapping.py
+++ library/commands/mapping.py
@@ -16,6 +16,7 @@ class MappingLayoutCommand(sublime_plugin.WindowCommand):
             sublime.error_message('File is not ERB file.')
             return
         self.project_dir = core.get_project_path(path)
+        self.view_dir = os.path.dirname(path);
         if self.project_dir is not None:
             for name in os.listdir(self.project_dir):
                 if core.is_erb_layout_file(name) is False:
@@ -30,9 +31,11 @@ class MappingLayoutCommand(sublime_plugin.WindowCommand):
         if index > -1:
             custom_layout = self.layout_list[index][0]
             filename = os.path.basename(self.window.active_view().file_name())
-            mapping_layout_file = os.path.join(self.project_dir, filename[:-3] + 'layout')
+            layout_relative = os.path.relpath(self.project_dir, os.path.dirname(self.window.active_view().file_name()));
+            layout_relative = '' if layout_relative == '.' else layout_relative
+            mapping_layout_file = os.path.join(self.view_dir, filename[:-3] + 'layout')
             f = open(mapping_layout_file, "w")
-            f.write(custom_layout)
+            f.write(os.path.join(layout_relative, custom_layout))
             f.close()
 
 class MappingPartialCommand(sublime_plugin.TextCommand):
